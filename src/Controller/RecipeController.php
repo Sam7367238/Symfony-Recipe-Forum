ef1c95eba8555048c26890616de500d1d326181e
Sam@Unique <aymantheremkus11@gmail.com>
2025-12-31 21:39:18 +0300

    Add Authorization For Recipes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route("/recipe", "recipe_")]
 final class RecipeController extends AbstractController
@@ -21,14 +22,15 @@ final class RecipeController extends AbstractController
     public function __construct(private EntityManagerInterface $entityManager) {}
 
     #[Route('/', name: 'index')]
-    public function index(Request $request, RecipeRepository $repository, PaginatorInterface $paginator): Response
+    public function index(Request $request, #[CurrentUser] ?User $user, RecipeRepository $repository, PaginatorInterface $paginator): Response
     {
+        $publicQuery = $this -> entityManager -> createQuery("SELECT r, u FROM App\Entity\Recipe r INNER JOIN r.user u WHERE r.private = false");
+        $userQuery = $repository -> createQueryBuilder("r") -> where("r.user = :id") -> setParameter("id", $user -> getId());
 
-        $query = $this -> entityManager -> createQuery("SELECT r, u FROM App\Entity\Recipe r INNER JOIN r.user u");
+        $publicRecipes = $paginator -> paginate($publicQuery, $request -> query -> getInt("page", 1), 5);
+        $userRecipes = $paginator -> paginate($userQuery, $request -> query -> getInt("page", 1), 5);
 
-        $recipes = $paginator -> paginate($query, $request -> query -> getInt("page", 1), 2);
-
-        return $this -> render("recipe/index.html.twig", compact("recipes"));
+        return $this -> render("recipe/index.html.twig", compact("publicRecipes", "userRecipes"));
     }
 
     #[Route('/new', "new")]
@@ -53,11 +55,13 @@ final class RecipeController extends AbstractController
     }
 
     #[Route("/{id<\d+>}", "show")]
+    #[IsGranted("RECIPE_MANAGE", "recipe")]
     public function show(Recipe $recipe, Request $request): Response {
         return $this -> render("recipe/show.html.twig", compact("recipe"));
     }
 
     #[Route('/{id<\d+>}/edit', "edit")]
+    #[IsGranted("RECIPE_MANAGE", "recipe")]
     public function edit(Request $request, #[CurrentUser] ?User $user, Recipe $recipe): Response {
         $form = $this -> createForm(RecipeType::class, $recipe);
         $form -> handleRequest($request);
@@ -74,6 +78,7 @@ final class RecipeController extends AbstractController
     }
 
     #[Route("/{id<\d+>}/delete", "delete")]
+    #[IsGranted("RECIPE_MANAGE", "recipe")]
     public function delete(Request $request, Recipe $recipe) {
         if ($request -> isMethod("POST")) {
             $this -> entityManager -> remove($recipe);
---
+++ src/Security/Voter/RecipeVoter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class RecipeVoter extends Voter
+{
+    public const MANAGE = 'RECIPE_MANAGE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::MANAGE])
+            && $subject instanceof \App\Entity\Recipe;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
+    {
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if (!$user instanceof User) {
+            $vote ?-> addReason("Unauthenticated");
+        }
+
+        return $user === $subject -> getUser() || !$subject -> isPrivate();
+    }
+}
